a86ecd806af7538375a83abb65e3b25348220cb4
Prashant M <pmathapa@mathworks.com>
2021-09-02 14:57:16 +0530

    Testing jenkins command with new options
    Signed-off-by: Prashant M <pmathapa@mathworks.com>

--- Wget/RUN_Command.bat
+++ Wget/RUN_Command.bat
@@ -1,22 +1,23 @@
 set PATH=C:\MATLAB\PS_Ser_R2021a\polyspace\bin;%PATH%
 set ps_helper_access=polyspace-access -host BGL-PMATHAPA -port 8085 -protocol http -login admin -encrypted-password PJJBJHMADIMOPCNPBDEJBPBJNENOAPCB
 set RESULT=ResultBF
-set PROG=DemoPaAcc
-set PARENT_PROJECT_ON_ACCESS=/public/DemoPaAcc
+set PROG=Testing
+set PARENT_PROJECT_ON_ACCESS=/public/Testing1
+set Options_Dir = C:\My_Drive\Wget\Options
 set WSP=%WORKSPACE%
 rd /S /Q Notification && md Notification
 
-set build_cmd=build.bat
+REM set build_cmd=build.bat
 
-polyspace-configure.exe -allow-overwrite -allow-build-error -prog %PROG% -author jenkins -output-options-file %PROG%.psopts %build_cmd% || EXIT /B 200
+REM polyspace-configure.exe -allow-overwrite -allow-build-error -prog %PROG% -author jenkins -output-options-file %PROG%.psopts %build_cmd% || EXIT /B 200
 
-polyspace-bug-finder-server.exe -options-file "%WSP%\Demo_1\target.opts" -options-file "%WSP%\Demo_1\%PROG%.psopts" -results-dir "%WSP%\R_BF_%BUILD_NUMBER%"   ||  EXIT /B 200
+polyspace-bug-finder-server.exe -options-file "%Options_Dir%\target.opts" -options-file "%Options_Dir%\wget_exe.psopts" -results-dir "%WSP%\R_BF"   ||  EXIT /B 200
 
 %ps_helper_access% -create-project %PARENT_PROJECT_ON_ACCESS%    ||  EXIT /B 200
 
-%ps_helper_access% -upload "%WSP%\R_BF_%BUILD_NUMBER%" -parent-project %PARENT_PROJECT_ON_ACCESS% -project "%PROG%_%BUILD_NUMBER%"	||  EXIT /B 200
+%ps_helper_access% -upload "%WSP%\R_BF" -parent-project %PARENT_PROJECT_ON_ACCESS% -project "%PROG%"	||  EXIT /B 200
 
-%ps_helper_access% -export %PARENT_PROJECT_ON_ACCESS%/%PROG%_%BUILD_NUMBER% -output Results_All.tsv -defects High    ||  EXIT /B 200
+%ps_helper_access% -export %PARENT_PROJECT_ON_ACCESS%/%PROG% -output Results_All.tsv -defects High    ||  EXIT /B 200
 
 exit 0
 
