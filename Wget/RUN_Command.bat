a036902aaa0ee694608db5a11e58f0490a26488f
Prashant M <pmathapa@mathworks.com>
2021-09-02 16:02:16 +0530

    Testing batch file
    Signed-off-by: Prashant M <pmathapa@mathworks.com>

--- Wget/RUN_Command.bat
+++ Wget/RUN_Command.bat
@@ -11,11 +11,11 @@ REM set build_cmd=build.bat
 
 REM polyspace-configure.exe -allow-overwrite -allow-build-error -prog %PROG% -author jenkins -output-options-file %PROG%.psopts %build_cmd% || EXIT /B 200
 
-polyspace-bug-finder-server.exe -options-file "%Options_Dir%\target.opts" -options-file "%Options_Dir%\wget_exe.psopts" -results-dir "%WSP%\R_BF"   ||  EXIT /B 200
+REM polyspace-bug-finder-server.exe -options-file "%Options_Dir%\target.opts" -options-file "%Options_Dir%\wget_exe.psopts" -results-dir "%WSP%\R_BF"   ||  EXIT /B 200
 
-%ps_helper_access% -create-project %PARENT_PROJECT_ON_ACCESS%    ||  EXIT /B 200
+REM %ps_helper_access% -create-project %PARENT_PROJECT_ON_ACCESS%    ||  EXIT /B 200
 
-%ps_helper_access% -upload "%WSP%\R_BF" -parent-project %PARENT_PROJECT_ON_ACCESS% -project "%PROG%"	||  EXIT /B 200
+REM %ps_helper_access% -upload "%WSP%\R_BF" -parent-project %PARENT_PROJECT_ON_ACCESS% -project "%PROG%"	||  EXIT /B 200
 
 %ps_helper_access% -export %PARENT_PROJECT_ON_ACCESS%/%PROG% -output Results_All.tsv -defects High    ||  EXIT /B 200
 
