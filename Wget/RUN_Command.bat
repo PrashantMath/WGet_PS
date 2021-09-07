22f7c1d95a3dbe4c3475d689712a5468f6cacf73
Prashant M <pmathapa@mathworks.com>
2021-09-07 06:39:22 +0530

    For Jenkins
    Signed-off-by: Prashant M <pmathapa@mathworks.com>

--- Wget/RUN_Command.bat
+++ Wget/RUN_Command.bat
@@ -4,7 +4,7 @@ set ps_helper_access=polyspace-access -host HYD-NALANDA-14 -port 8085 -protocol 
 REM Using it for demo purpose1
 
 set RESULT=ResultBF
-set PROG=Wget
+set PROG=Wget_Tested
 set PARENT_PROJECT_ON_ACCESS=/public/
 set Options_Dir=C:\My_Drive\Wget\Options
 set WSP=%WORKSPACE%
@@ -14,13 +14,13 @@ REM set build_cmd=build.bat
 
 REM polyspace-configure.exe -allow-overwrite -allow-build-error -prog %PROG% -author jenkins -output-options-file %PROG%.psopts %build_cmd% || EXIT /B 200
 
-REM polyspace-bug-finder-server.exe -options-file "%Options_Dir%\target.opts" -options-file "%Options_Dir%\wget_exe.psopts" -results-dir "%WSP%\R_BF"   ||  EXIT /B 200
+polyspace-bug-finder-server.exe -options-file "%Options_Dir%\target.opts" -options-file "%Options_Dir%\wget_exe.psopts" -results-dir "%WSP%\R_BF_1"   ||  EXIT /B 200
 
 cd ..
 
 %ps_helper_access% -create-project %PARENT_PROJECT_ON_ACCESS%    ||  EXIT /B 200
 
-%ps_helper_access% -upload "%WSP%\R_BF" -parent-project %PARENT_PROJECT_ON_ACCESS% -project "%PROG%"	||  EXIT /B 200
+%ps_helper_access% -upload "%WSP%\R_BF_1" -parent-project %PARENT_PROJECT_ON_ACCESS% -project "%PROG%"	||  EXIT /B 200
 
 %ps_helper_access% -export %PARENT_PROJECT_ON_ACCESS%/%PROG% -output Results_All.tsv -defects High    ||  EXIT /B 200
 
